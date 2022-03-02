bc566853ffed17807996f4e447721337b7d321be
Your Name <you@example.com>
2022-03-02 12:14:56 +0500

    Heroku uchun changed

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yxk%g0xg$1i#0wlu!2!^8h!ezhp@2khuy*ggz^0%g9mz$fg8q='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
