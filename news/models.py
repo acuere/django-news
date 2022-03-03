d46c6474144c8ba38a6c72982a8b8980e00f357d
Your Name <you@example.com>
2022-03-03 10:57:23 +0500

    Admin show in below post

--- news/models.py
+++ news/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField()
+    # created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return (self.title,
+                # f"({self.created_at:%Y-%m-%d %H:%M}):"
+                )
